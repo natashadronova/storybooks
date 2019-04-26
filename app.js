86b83d8b8f7ebd57377ddd90b1006f6cd6c9e992
Natalia Dronova <ndronova04@gmail.com>
2019-04-26 10:09:08 +1000

    added handlebars

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hola1')
+const express = require('express');
+const exphbs=require('express-handlebars');
+
+
+const app=express();
+
+//Handlebars middleware
+app.engine('handlebars',exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine','handlebars');
+
+
+//INDEX route
+app.get('/',(req,res)=>{
+  // res.send('INDEX');
+  res.render('index');
+});
+
+//ABOUT route
+app.get('/about',(req,res)=>{
+  res.send('ABOUT');
+});
+
+
+
+
+
+
+const port=5000;
+app.listen(port,()=>{
+  console.log(`Server started on port ${port}`);
+});
