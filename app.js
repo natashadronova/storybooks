56df2588d61551d67bc7526b9d4462f6427756cc
Natalia Dronova <ndronova04@gmail.com>
2019-04-28 11:36:55 +1000

    Added handlebars

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const exphbs=require('express-handlebars');
-
+const mongoose=require('mongoose');
 
 const app=express();
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev',{
+  useMongoClient:true
+})
+.then(()=>{
+console.log('mongodb connected')
+})
+.catch(err=>console.log(err));
+
+
 
 //Handlebars middleware
 app.engine('handlebars',exphbs({
@@ -13,13 +24,16 @@ app.set('view engine','handlebars');
 
 //INDEX route
 app.get('/',(req,res)=>{
-  // res.send('INDEX');
-  res.render('index');
+  const title='Welcome';
+  res.render('index',{
+    title: title
+  });
 });
 
 //ABOUT route
 app.get('/about',(req,res)=>{
-  res.send('ABOUT');
+  // res.send('ABOUT');
+  res.render('about');
 });
 
 
